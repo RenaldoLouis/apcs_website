7b80e66690366bd7ac81f401220068a22fceea33
RenaldoLouis <renald@10xers.co>
2024-01-20 18:07:11 +0700

    add galery

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "../../components/atom/navbar";
 import Content from "./Content";
+import Galery from "../Galery/Galery";
+import GalerySlider from "../Galery/GalerySlider";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
             <Content />
+            <Galery />
+            <GalerySlider />
             <div className="footer">
                 <p>Footer</p>
             </div>
