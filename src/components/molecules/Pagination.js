68ca5cd40b3e413ed26736f0c3f7da2c7be3a250
RenaldoLouis <renald@10xers.co>
2024-01-28 23:07:19 +0700

    set up firebase hosting and firebase stuff

--- src/components/molecules/Pagination.js
+++ src/components/molecules/Pagination.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { db } from '../../firebase';
 
 const Pagination = () => {
+    const [datas, setDatas] = useState([])
+
+    const fetchPost = async () => {
+        await getDocs(collection(db, "reviews"))
+            .then((querySnapshot) => {
+                const newData = querySnapshot.docs
+                    .map((doc) => ({ ...doc.data(), id: doc.id }));
+                setDatas(newData);
+            })
+    }
+
+    // useEffect(() => {
+    //     fetchPost();
+    // }, [])
 
     return (
         <div>
-
+            asd
         </div>
     )
 }
---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBOTI4bdDlZVOS7T5_LuDbu0nW9YpNNKkI",
+    authDomain: "apcs-profile.firebaseapp.com",
+    projectId: "apcs-profile",
+    storageBucket: "apcs-profile.appspot.com",
+    messagingSenderId: "290190992735",
+    appId: "1:290190992735:web:a9389660ac3aa2ed6542ea",
+    measurementId: "G-GS88QTZNJ9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+export const db = getFirestore(app);
