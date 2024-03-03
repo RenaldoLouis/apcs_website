69ea0d135a899acea8530728adc8629a5b4db050
RenaldoLouis <renald@10xers.co>
2024-03-03 19:45:43 +0700

    WIP better carousel

--- src/components/molecules/CarouselCustom.js
+++ src/components/molecules/CarouselCustom.js
@@ -24,34 +24,51 @@ const Carousel = ({ images, interval = 3000 }) => {
     };
 
     return (
-        <div className="carousel">
-            <div className="carousel-slides" style={{ '--currentIndex': currentIndex }}>
-                {images.map((image, index) => (
-                    <div
-                        key={index}
-                        className={`carousel-slide ${index === currentIndex ? 'active' : ''}`}
-                        style={{ backgroundImage: `url(${image})` }}
-                    ></div>
-                ))}
+        <>
+            <div className="carousel">
+                <div className="carousel-slides" style={{ '--currentIndex': currentIndex }}>
+                    {images.map((image, index) => (
+                        <div
+                            key={index}
+                            className={`carousel-slide ${index === currentIndex ? 'active' : ''}`}
+                            style={{ backgroundImage: `url(${image})` }}
+                        ></div>
+                    ))}
+                </div>
+                <div className="carousel-navigation">
+                    <button className="carousel-prev" onClick={goToPrevSlide}>
+                        &lt;
+                    </button>
+                    <button className="carousel-next" onClick={goToNextSlide}>
+                        &gt;
+                    </button>
+                </div>
+                <div className="carousel-dots">
+                    {images.map((_, index) => (
+                        <button
+                            key={index}
+                            className={`carousel-dot ${index === currentIndex ? 'active' : ''}`}
+                            onClick={() => goToSlide(index)}
+                        ></button>
+                    ))}
+                </div>
             </div>
-            <div className="carousel-navigation">
-                <button className="carousel-prev" onClick={goToPrevSlide}>
-                    &lt;
-                </button>
-                <button className="carousel-next" onClick={goToNextSlide}>
-                    &gt;
-                </button>
+
+
+
+            <div class="slider-wrapper">
+                <div class="slider">
+                    <img id="slide-1" src="https://images.unsplash.com/photo-1656464868371-602be27fd4c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" alt="3D rendering of an imaginary orange planet in space" />
+                    <img id="slide-2" src="https://images.unsplash.com/photo-1657586640569-4a3d4577328c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" alt="3D rendering of an imaginary green planet in space" />
+                    <img id="slide-3" src="https://images.unsplash.com/photo-1656077217715-bdaeb06bd01f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" alt="3D rendering of an imaginary blue planet in space" />
+                </div>
+                <div class="slider-nav">
+                    <a href="#slide-1"></a>
+                    <a href="#slide-2"></a>
+                    <a href="#slide-3"></a>
+                </div>
             </div>
-            <div className="carousel-dots">
-                {images.map((_, index) => (
-                    <button
-                        key={index}
-                        className={`carousel-dot ${index === currentIndex ? 'active' : ''}`}
-                        onClick={() => goToSlide(index)}
-                    ></button>
-                ))}
-            </div>
-        </div>
+        </>
     );
 };
 
