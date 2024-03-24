99e57892ba1abdd92c28511ab38935ac2e82a79f
RenaldoLouis <renald@10xers.co>
2024-03-24 14:28:05 +0700

    change peopleReviews to use contaiern row

--- src/Pages/Home/PeopleReviews.js
+++ src/Pages/Home/PeopleReviews.js
@@ -5,29 +5,30 @@ import PillButton from "../../components/atom/PillButton";
 const PeopleReviews = () => {
 
     return (
-        <div className="flex justify-evenly" style={{ height: 780 }}>
-            <div className="flex-column justify-around" >
-                <div style={{ display: 'flex', gap: 88 }}>
-                    <PeopleQuotes />
-                    <PeopleQuotes />
+        <div className="container" style={{ marginTop: 32, marginBottom: 32 }}>
+            <div className="row">
+                <div className="col">
+                    <div style={{ display: 'flex', gap: 88 }}>
+                        <PeopleQuotes />
+                        <PeopleQuotes />
+                    </div>
                 </div>
-                <div style={{ visibility: "hidden" }}>
-                    <PeopleQuotes />
-                </div>
+                <div className="col" style={{ textAlign: "center" }} />
             </div>
-            <div className="flex-column justify-around"  >
-                <div style={{ visibility: "hidden" }}>
-                    asdasd
-                </div>
-                <div className="flex-column">
-                    <div>
-                        Lorem Ipsum
-                    </div>
-                    <div style={{ width: 392 }}>
-                        Lorem ipsum dolor  sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt  ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </div>
-                    <div style={{ marginTop: 23 }}>
-                        <PillButton text="View More" isWhite={false} />
+
+            <div className="row">
+                <div className="col" />
+                <div className="col">
+                    <div className="flex-column" style={{ alignItems: "center" }}>
+                        <div>
+                            Lorem Ipsum
+                        </div>
+                        <div style={{ width: 392 }}>
+                            Lorem ipsum dolor  sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt  ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                        <div style={{ marginTop: 23 }}>
+                            <PillButton text="View More" isWhite={false} />
+                        </div>
                     </div>
                 </div>
             </div>
