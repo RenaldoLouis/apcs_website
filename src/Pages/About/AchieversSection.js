bd3b9985a1e626a23bc24cb5021ea786b1ce2b1b
RenaldoLouis <renald@10xers.co>
2024-04-23 09:46:31 +0700

    about layout done

--- src/Pages/About/AchieversSection.js
+++ src/Pages/About/AchieversSection.js
@@ -3,8 +3,22 @@ import React from "react";
 const AchieversSection = () => {
 
     return (
-        <div className="text-align-center" style={{ color: "white", background: "#646464" }}>
-            AchieversSection
+        <div style={{ color: "white", background: "#646464" }}>
+            <div className="container">
+                <div className="text-align-center" >
+                    Achievers
+                </div>
+
+                <div style={{ display: 'flex', justifyContent: "space-around" }}>
+                    <div>
+                        John DOe
+                    </div>
+
+                    <div>
+                        Lorem Iosum
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
