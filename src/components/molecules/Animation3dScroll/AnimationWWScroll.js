4b51554fc65bc7b40da53fa9ac130c288ba7f2fc
RenaldoLouis <renald@10xers.co>
2024-05-05 20:04:43 +0700

    fix scrolling problem in galery page

--- src/components/molecules/Animation3dScroll/AnimationWWScroll.js
+++ src/components/molecules/Animation3dScroll/AnimationWWScroll.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Preload, Scroll, ScrollControls } from "@react-three/drei"
 import { Canvas, useThree } from "@react-three/fiber"
 import { Suspense } from "react"
@@ -8,11 +8,23 @@ import ArrowUp from "../../../assets/icons/ArrowUp.png"
 
 const AnimationWWScroll = (props) => {
     const { setIsScrollDownAvailable } = props
+
+    useEffect(() => {
+        setTimeout(() => {
+            const animationScrollContainer = document.getElementById('animationScrollContainer');
+            let child = animationScrollContainer.children
+            let child2 = child[0].children[0].children
+            let child3 = child2[1]
+            child3.style.overscrollBehavior = "contain"
+            child3.style.overflow = "hidden auto"
+        }, 800);
+    }, [])
+
     return (
-        <div style={{ height: "100vh", width: "100vw", position: "relative" }}>
+        <div id="animationScrollContainer" style={{ height: "100vh", width: "100vw", position: "relative", overscrollBehaviorX: 'contain !important', overscrollBehaviorY: "contain !important" }}>
             <Canvas gl={{ antialias: false }} dpr={[1, 1.5]}>
                 <Suspense fallback={null}>
-                    <ScrollControls infinite horizontal pages={3} distance={1} style={{ overflow: "hidden !important" }}>
+                    <ScrollControls infinite horizontal pages={3} distance={1}>
                         <Scroll>
                             <Slides />
                         </Scroll>
