5f2f69f6bd5884c111315781fc751adf12aa706e
Renaldo Louis <renaldo.louis@commsult.id>
2025-01-19 17:54:02 +0700

    test add calender for assignemnt

--- src/Pages/AdminDashboard/AdminContent.js
+++ src/Pages/AdminDashboard/AdminContent.js
@@ -1,9 +1,11 @@
 import { Layout, Table, theme } from 'antd';
 import React, { useState } from 'react';
+import { ScheduleMeeting } from 'react-schedule-meeting';
 import { RegistrantsColumns } from '../../constant/RegistrantsColumn';
 import usePaginatedRegistrants from '../../hooks/useFetchRegistrantsData';
 
 const { Content } = Layout;
+
 const AdminContent = () => {
     const { token: { colorBgContainer, borderRadiusLG }, } = theme.useToken();
     const [isLoading, setIsLoading] = useState(false)
@@ -13,6 +15,14 @@ const AdminContent = () => {
     const handlePageChange = (pagination, filters, sorter, extra) => {
         setPage(pagination);
     };
+
+    const availableTimeslots = [0, 1, 2, 3, 4, 5].map((id) => {
+        return {
+            id,
+            startTime: new Date(new Date(new Date().setDate(new Date().getDate() + id)).setHours(9, 0, 0, 0)),
+            endTime: new Date(new Date(new Date().setDate(new Date().getDate() + id)).setHours(17, 0, 0, 0)),
+        };
+    });
 
     return (
         <Content
@@ -30,6 +40,13 @@ const AdminContent = () => {
             >
                 <Table columns={RegistrantsColumns} dataSource={registrantDatas} onChange={handlePageChange} />
             </div>
+            <ScheduleMeeting
+                borderRadius={10}
+                primaryColor="#3f5b85"
+                eventDurationInMinutes={30}
+                availableTimeslots={availableTimeslots}
+                onStartTimeSelect={console.log}
+            />
 
         </Content>
     )
