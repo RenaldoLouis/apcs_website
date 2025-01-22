e17a399c5ede11e5869e8bf685e88990eb790cec
RenaldoLouis <renaldolouis555@gmail.com>
2025-01-22 22:00:26 +0700

    fix upcomong podcast text

--- src/Pages/Podcast/Podcast.js
+++ src/Pages/Podcast/Podcast.js
@@ -1,8 +1,6 @@
 import React from "react";
 import podcastBacgkorund from "../../assets/images/podcastBacgkorund.jpg"
 import stayTuned from "../../assets/images/stayTuned.png"
-import musiciswhatapcs from "../../assets/images/musiciswhatapcs.svg"
-import BackgroundWithText from "../../components/molecules/BacgkroundWithText";
 import { ContentPosition } from "../../constant/ContentPosition";
 import CoverImageHome from "../../components/molecules/CoverImageHome";
 
